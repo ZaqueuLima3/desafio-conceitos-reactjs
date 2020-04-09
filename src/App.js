6a52465f61aac78c691a39ab73f10d7abd77673a
zaqueu lima dos santos <zaqueulima1@gmail.com>
2020-04-09 17:07:56 -0300

    Repository CRUD front

--- src/App.js
+++ src/App.js
@@ -1,29 +1,100 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from "./services/api"
 
 import "./styles.css";
 
 function App() {
-  async function handleAddRepository() {
-    // TODO
+  const [repositories, setRepositories] = useState([]);
+  const [title, setTitle] = useState("");
+  const [url, setUrl] = useState("");
+  const [techs, setTechs] = useState("");
+
+  useEffect(() => {
+    getRepos();
+  }, [])
+
+  async function getRepos() {
+    const response = await api.get("repositories");
+
+    setRepositories(response.data);
+  }
+
+  async function handleAddRepository(event) {
+    event.preventDefault();
+
+    const newRepo = {
+      url,
+      title,
+      techs: techs.split(","),
+    };
+
+    const response = await api.post("repositories", newRepo);
+
+    setRepositories([...repositories, response.data]);
+
+    setTitle("");
+    setUrl("");
+    setTechs("");
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`repositories/${id}`)
+
+    const repositoriesFiltered = repositories.filter(
+      repository => repository.id !== id
+    )
+
+    setRepositories(repositoriesFiltered)
   }
 
   return (
     <div>
-      <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+      <form action="">
+        <label htmlFor="title">Titulo</label>
+        <input 
+          type="text" 
+          id="title" name="title"
+          placeholder="Desafio ReactJS"
+          value={title}
+          onChange={(event) => setTitle(event.target.value)}
+        />
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        <label htmlFor="url">Url</label>
+        <input 
+          type="text"
+          id="url"
+          name="url"
+          placeholder="https://github.com/josepholiveira"
+          value={url}
+          onChange={(event) => setUrl(event.target.value)}
+        />
+
+        <label htmlFor="techs">Tecnologias</label>
+        <input 
+          type="text"
+          id="techs"
+          name="techs"
+          placeholder="React, Node, GraphQL"
+          value={techs}
+          onChange={(event) => setTechs(event.target.value)}
+        />
+        
+        <button onClick={handleAddRepository}>Adicionar</button>
+      </form>
+    
+      <ul data-testid="repository-list">
+        {repositories.map(repository => (
+          <li key={repository.id}>
+            <p>{repository.title}</p>
+
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
+            </button>
+          </li>
+
+        ))}
       </ul>
 
-      <button onClick={handleAddRepository}>Adicionar</button>
     </div>
   );
 }
